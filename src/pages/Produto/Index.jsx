27f8f98f3bf4b0541b4d5129710650bd8d9c4b67
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-11-01 07:32:44 -0300

    Commit necessário

--- src/pages/Produto/Index.jsx
+++ src/pages/Produto/Index.jsx
@@ -1,5 +1,5 @@
 import './Index.scss';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Head from '../../components/header/Index';
@@ -42,17 +42,22 @@ export default function Produto() {
       <div className="page-center">
         <input type="text" placeholder='  O que procura?' />
         <div className='img-text'>
-          <img src={produto.imagem || "/assets/image/bolo.jpeg"} alt={produto.nome} />
-          <div className="description-product">
-            <h1>{produto.nome}</h1>
-            <h3>R${produto.valor}</h3>
-            <p>{produto.descrição}</p>
-          </div>
-          <div className="direct-whatsapp">
-            <img src="/assets/image/whatsapp.png" alt="WhatsApp" />
-            <h3>Fale conosco no WhatsApp</h3>
+          <img className='img-bolo' src={produto.imagem || "/assets/image/bolo.jpeg"} alt={produto.nome} />
+          <div className="descrissoes">
+            <div className="description-product">
+              <h1>{produto.nome}</h1>
+              <h3>R${produto.valor}</h3>
+              <p>{produto.descrição}</p>
+            </div>
+            <div>
+              <Link to="https://w.app/VZDyK5" className='direct-whatsapp'>
+                <img src="/assets/image/whatsapp-black.png" alt="WhatsApp" />
+                <h3>Clique aqui!Para finalizar <br /> seu pedido pelo Whatsapp!</h3>
+              </Link>
+            </div>
           </div>
         </div>
+        <div className='line'></div>
       </div>
       <Baseboard />
     </div>
